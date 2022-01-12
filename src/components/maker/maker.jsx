befb5d03199fabbd62631c757e26cb22cfb3288e
ohk9412 <ohk9412@naver.com>
2022-01-12 14:10:58 +0900

    use firebase realtime database

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -21,6 +21,16 @@ const Maker = ({FileInput , authService, cardRepository}) => {
         authService.logout();
     };
 
+    useEffect(()=> {
+        if (!userId) {
+            return;
+        }
+        const stopSync = cardRepository.syncCards(userId,cards => {
+            setCards(cards);
+        });
+        return() => stopSync();
+    },[userId]);
+
     useEffect(() => {
         authService.onAuthChange(user => {
             if(user) {
